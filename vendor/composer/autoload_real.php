e06b6d89e3176c97fded436fc97a360434807939
Abdur Rahaman <aoradev03@gmail.com>
2026-04-28 02:57:30 +0600

    initial commit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,6 @@ class ComposerAutoloaderInit212d7353d2270db48231d62ba234ffb7
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit212d7353d2270db48231d62ba234ffb7::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }
